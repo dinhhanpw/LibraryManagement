d1b355a737ad8207b94dd3bee71fd096e5d2c998
dinhhanpw <55496521+dinhhanpw@users.noreply.github.com>
2020-06-26 15:34:32 +0700

    fix bug

--- LibraryManagement/LibraryManagement/ViewModel/MainViewModel.cs
+++ LibraryManagement/LibraryManagement/ViewModel/MainViewModel.cs
@@ -8,9 +8,9 @@ namespace LibraryManagement.ViewModel
 
         public MainViewModel()
         {
-            Items.Add(new NavigationItem() { Title = "Quản Lí Đọc Giả", IconGlyph = "&#xe801", Content = null });
-            Items.Add(new NavigationItem() { Title = "Quản Lí Sách", IconGlyph = "&#xe651", Content = null });
-            Items.Add(new NavigationItem() { Title = "Quản Lí Mượn Trả Sách", IconGlyph = "&#xe649", Content = null });
+            Items.Add(new NavigationItem() { Title = "Quản Lý Đọc Giả", IconGlyph = "&#xe801", Content = null });
+            Items.Add(new NavigationItem() { Title = "Quản Lý Sách", IconGlyph = "&#xe651", Content = null });
+            Items.Add(new NavigationItem() { Title = "Quản Lý Mượn Trả Sách", IconGlyph = "&#xe649", Content = null });
             Items.Add(new NavigationItem() { Title = "Quy Định", IconGlyph = "&#xe401", Content = null });
         }
     }
